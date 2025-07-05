4dd528ceb8653769320bf286af0c0f054a307850
Shyaka Chaste <121980393+shyakachaste@users.noreply.github.com>
2025-07-05 16:51:59 +0200

    updated

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
 import './AboutSection.css';
+import AboutImage from '../assets/about.jpg';
 
-const AboutSection = () => {
+const AboutUs = () => {
+
   return (
-    <section className="about">
-      <div className="about-right">
-        <h2>About Hack With Me</h2>
+    <section className="about-section">
+      <div className="about-container">
+        <div className="about-content">
+          <div className="text-section">
+            <div className="about-badge">ABOUT</div>
+            <h1 className="about-title">We build awesome things together.</h1>
+            
+            <div className="about-description">
+              <p>
+               Hack With Me is an annual 24-hour coding hackathon designed for high schoolers. 
+               Students come together to build projects, solve problems, and share their skills with others.
+              </p>
+              <p>
+               The goal of Hack With Me is to make hackathons accessible to everyone — even if you don’t have a team or idea, 
+               you’ll still be able to build something great with the help of mentors and workshops.
+              </p>
+              <p>
+               Hack With Me is the largest hackathon for high schoolers in Rwanda and across Africa, 
+               bringing students together to learn, build, and connect.
+              </p>
+
+            </div>
+            <div className="contact-section">
+              <span className="contact-text">For any inquiries, contact us at:</span>
+              <a href="mailto:hello@hackwithme.com" className="contact-email">
+                hello@hackwithme.com
+              </a>
+            </div>
+          </div>
+
+          <div className="image-section">
+            <div className="hackathon-badge">
+                 <img src={AboutImage} alt="About Hack With Me" className="badge-full-image" />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
 };
 
-export default AboutSection;
-
+export default AboutUs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import AboutUs from '../components/AboutUs';
+import Counting from '../components/Counting';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <AboutUs />
+      <Counting />
     </>
   );
 };
