6fd501f0165af94a6ba7140cb1943e7cf542b221
Shyaka Chaste <121980393+shyakachaste@users.noreply.github.com>
2025-08-26 02:06:52 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const hasPrerender = container && container.hasChildNodes();
+
+if (hasPrerender) {
+  // hydrate if react-snap/SSR pre-rendered HTML exists
+  import('react-dom').then(({ hydrate }) => {
+    hydrate(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      container
+    );
+  });
+} else {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 reportWebVitals();
